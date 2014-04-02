9337a4702e7fd3632cae8681b8539dacd24c240d
dipesh kakadiya <dipesh.kakadiya@rtcamp.com>
2014-04-02 21:02:06 +0530

    calendar helper class added.

--- app/class-rt-wp-hrm.php
+++ app/class-rt-wp-hrm.php
@@ -69,7 +69,7 @@ if ( ! class_exists( 'RT_WP_HRM' ) ) {
 		<?php }
 
 		function init_globals() {
-			global $rt_hrm_plugin_info, $rt_hrm_roles, $rt_hrm_module, $rt_hrm_dashboard, $rt_hrm_calendar;
+			global $rt_hrm_plugin_info, $rt_hrm_roles, $rt_hrm_module, $rt_hrm_dashboard, $rt_hrm_calendar, $rt_calendar;
 
 			$rt_hrm_plugin_info = new RT_Plugin_Info( trailingslashit( RT_HRM_PATH ) . 'index.php' );
 
@@ -77,6 +77,7 @@ if ( ! class_exists( 'RT_WP_HRM' ) ) {
 			$rt_hrm_roles = new Rt_HRM_Roles();
 			$rt_hrm_dashboard = new Rt_HRM_Dashboard();
 			$rt_hrm_calendar = new Rt_HRM_Calendar();
+			$rt_calendar = new RT_Calendar();
 
 		}
 
