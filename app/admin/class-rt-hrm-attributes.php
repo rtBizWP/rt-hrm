05754682c9e775d1e88fb3a19d1b5124a0382920
dipesh kakadiya <dipesh.kakadiya@rtcamp.com>
2014-04-08 18:45:42 +0530

    htm-attributes added.

--- app/admin/class-rt-hrm-attributes.php
+++ app/admin/class-rt-hrm-attributes.php
@@ -34,7 +34,7 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 				'delete_terms' => $rt_hrm_roles->global_caps['delete_rthrm_terms'],
 				'assign_terms' => $rt_hrm_roles->global_caps['assign_rthrm_terms'],
 			);
-			$rt_attributes->add_attributes_page( $this->attributes_page_slug, 'edit.php?post_type='.$rt_hrm_module->post_type, $rt_hrm_module->post_type, $rt_hrm_roles->global_caps['manage_attributes'], $terms_caps, $render_type = true, $storage_type = false, $orderby = true );
+			$rt_attributes->add_attributes_page( $this->attributes_page_slug, 'edit.php?post_type='.$rt_hrm_module->post_type, '', $rt_hrm_roles->global_caps['manage_attributes'], $terms_caps, $render_type = true, $storage_type = true, $orderby = true );
 			$rt_attributes_model = new RT_Attributes_Model();
 			$rt_attributes_relationship_model = new RT_Attributes_Relationship_Model();
 		}
@@ -50,7 +50,7 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 					$diffHTML = $this->meta_diff( $attr, $post_id, $newLead );
 					break;
 				default:
-					$diffHTML = apply_filters( 'rtcrm_attribute_diff', $diffHTML, $attr, $post_id, $newLead );
+					$diffHTML = apply_filters( 'rthrm_attribute_diff', $diffHTML, $attr, $post_id, $newLead );
 					break;
 			}
 			return $diffHTML;
@@ -69,7 +69,7 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 					$newVals = $newLead[$attr->attribute_name];
 					$newVals = array_unique($newVals);
 
-					$get_post_terms = wp_get_post_terms( $post_id, rtcrm_attribute_taxonomy_name( $attr->attribute_name ) );
+					$get_post_terms = wp_get_post_terms( $post_id, rthrm_attribute_taxonomy_name( $attr->attribute_name ) );
 					if ( $get_post_terms ) {
 						$post_term_slug = $get_post_terms[0]->term_id;
 						$post_term_name = $get_post_terms[0]->name;
@@ -78,14 +78,14 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 						$post_term_name = '';
 					}
 					if ( !empty( $newVals ) ) {
-						$newTerms = get_term_by( 'id', $newVals[0], rtcrm_attribute_taxonomy_name( $attr->attribute_name ) );
+						$newTerms = get_term_by( 'id', $newVals[0], rthrm_attribute_taxonomy_name( $attr->attribute_name ) );
 						$post_new_term_slug = $newVals[0];
 						$post_new_term_name = $newTerms->name;
 					} else {
 						$post_new_term_slug = '';
 						$post_new_term_name = '';
 					}
-					$diff = rtcrm_text_diff( $post_term_name, $post_new_term_name );
+					$diff = rthrm_text_diff( $post_term_name, $post_new_term_name );
 					if ( $diff ) {
 						$diffHTML .= '<tr><th style="padding: .5em;border: 0;">'.$attr->attribute_label.'</th><td>' . $diff . '</td><td></td></tr>';
 					}
@@ -96,23 +96,23 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 					}
 					$newVals = $newLead[$attr->attribute_name];
 					$newVals = array_unique( $newVals );
-					$oldTermString = rtcrm_post_term_to_string( $post_id, rtcrm_attribute_taxonomy_name( $attr->attribute_name ) );
+					$oldTermString = rthrm_post_term_to_string( $post_id, rthrm_attribute_taxonomy_name( $attr->attribute_name ) );
 					$newTermString = '';
 					if(!empty($newVals)) {
 						$newTermArr = array();
 						foreach ( $newVals as $value ) {
-							$newTerm = get_term_by( 'id', $value, rtcrm_attribute_taxonomy_name( $attr->attribute_name ) );
+							$newTerm = get_term_by( 'id', $value, rthrm_attribute_taxonomy_name( $attr->attribute_name ) );
 							$newTermArr[] = $newTerm->name;
 						}
 						$newTermString = implode(',', $newTermArr);
 					}
-					$diff = rtcrm_text_diff( $oldTermString, $newTermString );
+					$diff = rthrm_text_diff( $oldTermString, $newTermString );
 					if ( $diff ) {
 						$diffHTML .= '<tr><th style="padding: .5em;border: 0;">'.$attr->attribute_label.'</th><td>' . $diff . '</td><td></td></tr>';
 					}
 					break;
 				default:
-					$diffHTML = apply_filters( 'rtcrm_attribute_diff', $diffHTML, $attr, $post_id, $newLead );
+					$diffHTML = apply_filters( 'rthrm_attribute_diff', $diffHTML, $attr, $post_id, $newLead );
 					break;
 			}
 			return $diffHTML;
@@ -123,27 +123,25 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 
 			$oldattr = get_post_meta( $post_id, $attr->attribute_name, true );
 			if ( $oldattr != $newLead[$attr->attribute_name] ) {
-				$diffHTML .= '<tr><th style="padding: .5em;border: 0;">'.$attr->attribute_label.'</th><td>' . rtcrm_text_diff( $oldattr, $newLead[$attr->attribute_name] ) . '</td><td></td></tr>';
+				$diffHTML .= '<tr><th style="padding: .5em;border: 0;">'.$attr->attribute_label.'</th><td>' . rthrm_text_diff( $oldattr, $newLead[$attr->attribute_name] ) . '</td><td></td></tr>';
 			}
 			update_post_meta($post_id, $attr->attribute_name, $newLead[$attr->attribute_name]);
 			return $diffHTML;
 		}
 
 		function save_attributes( $attr, $post_id, $newLead ) {
-			var_dump(  $attr->attribute_store_as  );
 			switch ( $attr->attribute_store_as ) {
 				case 'taxonomy':
 					if ( !isset( $newLead[$attr->attribute_name] ) ) {
 						$newLead[$attr->attribute_name] = array();
 					}
-					wp_set_post_terms( $post_id, implode( ',', $newLead[$attr->attribute_name] ), rtcrm_attribute_taxonomy_name( $attr->attribute_name ) );
+					wp_set_post_terms( $post_id, implode( ',', $newLead[$attr->attribute_name] ), rthrm_attribute_taxonomy_name( $attr->attribute_name ) );
 					break;
 				case 'meta':
-					var_dump( "hi" );
 					update_post_meta( $post_id, $attr->attribute_name, $newLead[$attr->attribute_name] );
 					break;
 				default:
-					do_action( 'rtcrm_update_attribute', $attr, $post_id, $newLead );
+					do_action( 'rthrm_update_attribute', $attr, $post_id, $newLead );
 					break;
 			}
 		}
@@ -157,7 +155,7 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 					$this->render_meta( $attr, $post_id, $edit );
 					break;
 				default:
-					do_action('rtcrm_render_attribute', $attr, $post_id, $edit );
+					do_action('rthrm_render_attribute', $attr, $post_id, $edit );
 					break;
 			}
 		}
@@ -168,8 +166,8 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 //					break;
 				case 'dropdown':
 					$options = array();
-					$terms = get_terms( rtcrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'hide_empty' => false, 'orderby' => $attr->attribute_orderby, 'order' => 'asc' ) );
-					$post_term = wp_get_post_terms($post_id, rtcrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'fields' => 'ids' ) );
+					$terms = get_terms( rthrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'hide_empty' => false, 'orderby' => $attr->attribute_orderby, 'order' => 'asc' ) );
+					$post_term = wp_get_post_terms($post_id, rthrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'fields' => 'ids' ) );
 					// Default Selected Term for the attribute. can beset via settings -- later on
 					$selected_term = '-11111';
 					if( !empty( $post_term ) ) {
@@ -184,14 +182,14 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 					if( $edit ) {
 						$this->render_dropdown( $attr, $options );
 					} else {
-						$term = get_term( $selected_term, rtcrm_attribute_taxonomy_name( $attr->attribute_name ) );
-						echo '<span class="rtcrm_view_mode">'.$term->name.'</span>';
+						$term = get_term( $selected_term, rthrm_attribute_taxonomy_name( $attr->attribute_name ) );
+						echo '<span class="rthrm_view_mode">'.$term->name.'</span>';
 					}
 					break;
 				case 'checklist':
 					$options = array();
-					$terms = get_terms( rtcrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'hide_empty' => false, 'orderby' => $attr->attribute_orderby, 'order' => 'asc' ) );
-					$post_terms = wp_get_post_terms($post_id, rtcrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'fields' => 'ids' ) );
+					$terms = get_terms( rthrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'hide_empty' => false, 'orderby' => $attr->attribute_orderby, 'order' => 'asc' ) );
+					$post_terms = wp_get_post_terms($post_id, rthrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'fields' => 'ids' ) );
 					if ( empty( $post_terms ) ) {
 						$post_terms = array();
 					}
@@ -210,13 +208,13 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 								$selected_terms[] = $term->name;
 							}
 						}
-						echo '<span class="rtcrm_view_mode">'.  implode( ',', $selected_terms ).'</span>';
+						echo '<span class="rthrm_view_mode">'.  implode( ',', $selected_terms ).'</span>';
 					}
 					break;
 				case 'rating-stars':
 					$options = array();
-					$terms = get_terms( rtcrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'hide_empty' => false, 'orderby' => $attr->attribute_orderby, 'order' => 'asc' ) );
-					$post_term = wp_get_post_terms($post_id, rtcrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'fields' => 'ids' ) );
+					$terms = get_terms( rthrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'hide_empty' => false, 'orderby' => $attr->attribute_orderby, 'order' => 'asc' ) );
+					$post_term = wp_get_post_terms($post_id, rthrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'fields' => 'ids' ) );
 					// Default Selected Term for the attribute. can beset via settings -- later on
 					$selected_term = '-11111';
 					if( !empty( $post_term ) ) {
@@ -232,12 +230,12 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 					if( $edit ) {
 						$this->render_rating_stars( $attr, $options );
 					} else {
-						$term = get_term( $selected_term, rtcrm_attribute_taxonomy_name( $attr->attribute_name ) );
-						echo '<span class="rtcrm_view_mode">'.$term->name.'</span>';
+						$term = get_term( $selected_term, rthrm_attribute_taxonomy_name( $attr->attribute_name ) );
+						echo '<span class="rthrm_view_mode">'.$term->name.'</span>';
 					}
 					break;
 				default:
-					do_action( 'rtcrm_render_taxonomy', $attr, $post_id, $edit );
+					do_action( 'rthrm_render_taxonomy', $attr, $post_id, $edit );
 					break;
 			}
 		}
@@ -246,8 +244,8 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 			switch ( $attr->attribute_render_type ) {
 				case 'dropdown':
 					$options = array();
-					$terms = get_terms( rtcrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'hide_empty' => false, 'orderby' => $attr->attribute_orderby, 'order' => 'asc' ) );
-					$post_term = wp_get_post_terms($post_id, rtcrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'fields' => 'ids' ) );
+					$terms = get_terms( rthrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'hide_empty' => false, 'orderby' => $attr->attribute_orderby, 'order' => 'asc' ) );
+					$post_term = wp_get_post_terms($post_id, rthrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'fields' => 'ids' ) );
 					// Default Selected Term for the attribute. can beset via settings -- later on
 					$selected_term = '-11111';
 					if( !empty( $post_term ) ) {
@@ -259,17 +257,17 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 							'selected' => ($term->term_id == $selected_term) ? true : false,
 						);
 					} ?>
-					<div class="large-4 small-4 columns <?php echo ( ! $edit ) ? 'rtcrm_attr_border' : ''; ?>">
+					<div class="large-4 small-4 columns <?php echo ( ! $edit ) ? 'rthrm_attr_border' : ''; ?>">
 						<span class="prefix" title="<?php echo $attr->attribute_label; ?>"><label for="post[<?php echo $attr->attribute_name; ?>]"><?php echo $attr->attribute_label; ?></label></span>
 					</div>
 					<div class="large-8 mobile-large-2 columns">
-						<?php if( $edit ) { $this->render_dropdown( $attr, $options ); } else { $term = get_term( $selected_term, rtcrm_attribute_taxonomy_name( $attr->attribute_name ) ); echo '<span class="rtcrm_view_mode">'.$term->name.'</span>'; } ?>
+						<?php if( $edit ) { $this->render_dropdown( $attr, $options ); } else { $term = get_term( $selected_term, rthrm_attribute_taxonomy_name( $attr->attribute_name ) ); echo '<span class="rthrm_view_mode">'.$term->name.'</span>'; } ?>
 					</div>
 					<?php break;
 				case 'rating-stars':
 					$options = array();
-					$terms = get_terms( rtcrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'hide_empty' => false, 'orderby' => $attr->attribute_orderby, 'order' => 'asc' ) );
-					$post_term = wp_get_post_terms($post_id, rtcrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'fields' => 'ids' ) );
+					$terms = get_terms( rthrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'hide_empty' => false, 'orderby' => $attr->attribute_orderby, 'order' => 'asc' ) );
+					$post_term = wp_get_post_terms($post_id, rthrm_attribute_taxonomy_name( $attr->attribute_name ), array( 'fields' => 'ids' ) );
 					// Default Selected Term for the attribute. can beset via settings -- later on
 					$selected_term = '-11111';
 					if( !empty( $post_term ) ) {
@@ -286,45 +284,32 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 					<div class="large-4 small-4 columns">
 						<span class="prefix" title="<?php echo $attr->attribute_label; ?>"><label for="post[<?php echo $attr->attribute_name; ?>]"><?php echo $attr->attribute_label; ?></label></span>
 					</div>
-					<div class="large-8 mobile-large-2 columns rtcrm_attr_border">
-						<?php if( $edit ) { $this->render_rating_stars( $attr, $options ); } else { $term = get_term( $selected_term, rtcrm_attribute_taxonomy_name( $attr->attribute_name ) ); echo '<div class="rtcrm_attr_border rtcrm_view_mode">'.$term->name.'</div>'; } ?>
+					<div class="large-8 mobile-large-2 columns rthrm_attr_border">
+						<?php if( $edit ) { $this->render_rating_stars( $attr, $options ); } else { $term = get_term( $selected_term, rthrm_attribute_taxonomy_name( $attr->attribute_name ) ); echo '<div class="rthrm_attr_border rthrm_view_mode">'.$term->name.'</div>'; } ?>
 					</div>
 					<?php break;
 				case 'date':
 					$value = get_post_meta( $post_id, $attr->attribute_name, true ); ?>
-					<div class="large-4 mobile-large-1 columns">
-						<span class="prefix" title="<?php echo $attr->attribute_label; ?>"><label for="post[<?php echo $attr->attribute_name; ?>]"><?php echo $attr->attribute_label; ?></label></span>
+					<div class="large-7 mobile-large-2 columns <?php echo ( ! $edit ) ? 'rthrm_attr_border' : ''; ?>">
+						<?php if( $edit ) { $this->render_date( $attr, $value ); } else { echo '<span class="rthrm_view_mode moment-from-now">'.$value.'</span>'; } ?>
 					</div>
-					<div class="large-7 mobile-large-2 columns <?php echo ( ! $edit ) ? 'rtcrm_attr_border' : ''; ?>">
-						<?php if( $edit ) { $this->render_date( $attr, $value ); } else { echo '<span class="rtcrm_view_mode moment-from-now">'.$value.'</span>'; } ?>
-					</div>
-					<?php if( $edit ) { ?>
-					<div class="large-1 mobile-large-1 columns">
-						<span class="postfix datepicker-toggle" data-datepicker="<?php echo $attr->attribute_name; ?>"><label class="foundicon-calendar"></label></span>
-					</div>
-				<?php }
+					<?php
 					break;
 				case 'datetime':
 					$value = get_post_meta( $post_id, $attr->attribute_name, true ); ?>
-					<div class="large-4 mobile-large-1 columns">
-						<span class="prefix" title="<?php echo $attr->attribute_label; ?>"><label for="post[<?php echo $attr->attribute_name; ?>]"><?php echo $attr->attribute_label; ?></label></span>
+
+					<div class="large-7 mobile-large-2 columns <?php echo ( ! $edit ) ? 'rthrm_attr_border' : ''; ?>">
+						<?php if( $edit ) { $this->render_datetime( $attr, $value ); } else { echo '<span class="rthrm_view_mode moment-from-now">'.$value.'</span>'; } ?>
 					</div>
-					<div class="large-7 mobile-large-2 columns <?php echo ( ! $edit ) ? 'rtcrm_attr_border' : ''; ?>">
-						<?php if( $edit ) { $this->render_datetime( $attr, $value ); } else { echo '<span class="rtcrm_view_mode moment-from-now">'.$value.'</span>'; } ?>
-					</div>
-					<?php if( $edit ) { ?>
-					<div class="large-1 mobile-large-1 columns">
-						<span class="postfix datetimepicker-toggle" data-datetimepicker="<?php echo $attribute_name; ?>"><label class="foundicon-calendar"></label></span>
-					</div>
-				<?php }
+					<?php
 					break;
 				case 'currency':
 					$value = get_post_meta( $post_id, $attr->attribute_name, true ); ?>
 					<div class="large-4 mobile-large-1 columns">
 						<span class="prefix" title="<?php echo $attr->attribute_label; ?>"><label for="post[<?php echo $attr->attribute_name; ?>]"><?php echo $attr->attribute_label; ?></label></span>
 					</div>
-					<div class="large-7 mobile-large-2 columns <?php echo ( ! $edit ) ? 'rtcrm_attr_border' : ''; ?>">
-						<?php if( $edit ) { $this->render_currency( $attr, $value ); } else { echo '<span class="rtcrm_view_mode">'.$value.'</span>'; } ?>
+					<div class="large-7 mobile-large-2 columns <?php echo ( ! $edit ) ? 'rthrm_attr_border' : ''; ?>">
+						<?php if( $edit ) { $this->render_currency( $attr, $value ); } else { echo '<span class="rthrm_view_mode">'.$value.'</span>'; } ?>
 					</div>
 					<?php if( $edit ) { ?>
 					<div class="large-1 mobile-large-1 columns">
@@ -337,12 +322,12 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 					<div class="large-4 small-4 columns">
 						<span class="prefix" title="<?php echo $attr->attribute_label; ?>"><label for="post[<?php echo $attr->attribute_name; ?>]"><?php echo $attr->attribute_label; ?></label></span>
 					</div>
-					<div class="large-8 mobile-large-2 columns <?php echo ( ! $edit ) ? 'rtcrm_attr_border' : ''; ?>">
-						<?php if( $edit ) { $this->render_text( $attr, $value ); } else { echo '<span class="rtcrm_view_mode">'.$value.'</span>'; } ?>
+					<div class="large-8 mobile-large-2 columns <?php echo ( ! $edit ) ? 'rthrm_attr_border' : ''; ?>">
+						<?php if( $edit ) { $this->render_text( $attr, $value ); } else { echo '<span class="rthrm_view_mode">'.$value.'</span>'; } ?>
 					</div>
 					<?php break;
 				default:
-					do_action( 'rtcrm_render_meta', $attr, $post_id, $edit );
+					do_action( 'rthrm_render_meta', $attr, $post_id, $edit );
 					break;
 			}
 		}
@@ -363,7 +348,7 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 			$args = array(
 				'id' => $attr->attribute_name,
 				'name' => 'post['.$attr->attribute_name.'][]',
-				'class' => array('rtcrm-stars'),
+				'class' => array('rthrm-stars'),
 				'misc' => array(
 					'class' => 'star',
 				),
@@ -411,7 +396,7 @@ if( !class_exists( 'Rt_HRM_Attributes' ) ) {
 			$args = array(
 				'id' => $attr->attribute_name,
 				'class' => array(
-					'datetimepicker',
+					'datepicker',
 					'moment-from-now',
 				),
 				'misc' => array(
