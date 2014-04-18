71c9fed3ef9074c10b4ae4f3d34499b598171d63
Udit Desai <desaiuditd@gmail.com>
2014-04-18 20:56:35 +0530

    Admin Menu Page Priority Fix

--- app/admin/class-rt-hrm-module.php
+++ app/admin/class-rt-hrm-module.php
@@ -523,6 +523,7 @@ if( !class_exists( 'Rt_HRM_Module' ) ) {
                 'rthrm-'.$this->post_type.'-calendar',
                 'edit.php?post_type='.$this->post_type,
                 'post-new.php?post_type='.$this->post_type,
+				'edit-tags.php?taxonomy='.Rt_HRM_Attributes::$leave_type_tax.'&amp;post_type='.$this->post_type,
 				RT_WP_HRM::$settings_page_slug,
                 $rt_hrm_attributes->attributes_page_slug,
             );
@@ -571,6 +572,9 @@ if( !class_exists( 'Rt_HRM_Module' ) ) {
                     foreach( $this->custom_menu_order as $item ){
                         foreach ( $module_menu as $p_key => $menu_item ){
                             if ( in_array( $item, $menu_item ) ) {
+								if ( $item == 'edit-tags.php?taxonomy='.Rt_HRM_Attributes::$leave_type_tax.'&amp;post_type='.$this->post_type ) {
+									$menu_item[0]= '--- '.$menu_item[0];
+								}
                                 $submenu['edit.php?post_type='.$this->post_type][$new_index] = $menu_item;
                                 unset ( $module_menu[$p_key] );
                                 $new_index += 5;
@@ -578,12 +582,12 @@ if( !class_exists( 'Rt_HRM_Module' ) ) {
                             }
                         }
                     }
-                    foreach( $module_menu as $p_key => $menu_item ){
-                        $menu_item[0]= '--- '.$menu_item[0];
-                        $submenu['edit.php?post_type='.$this->post_type][$new_index] = $menu_item;
-                        unset ( $module_menu[$p_key] );
-                        $new_index += 5;
-                    }
+//                    foreach( $module_menu as $p_key => $menu_item ){
+//                        $menu_item[0]= '--- '.$menu_item[0];
+//                        $submenu['edit.php?post_type='.$this->post_type][$new_index] = $menu_item;
+//                        unset ( $module_menu[$p_key] );
+//                        $new_index += 5;
+//                    }
                 }
             }
             return $menu_order;
