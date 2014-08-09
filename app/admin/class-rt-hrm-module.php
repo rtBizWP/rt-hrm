c21c26b0a9b9d87642c10dd74e73592f1040335b
Udit Desai <desaiuditd@gmail.com>
2014-08-09 14:00:08 +0530

    HRM Help Changes

--- app/admin/class-rt-hrm-module.php
+++ app/admin/class-rt-hrm-module.php
@@ -66,7 +66,6 @@ if( !class_exists( 'Rt_HRM_Module' ) ) {
         public function __construct() {
 			$this->get_custom_statuses();
             $this->get_custom_menu_order();
-              $this->init_help();
 			add_action( 'init', array( $this, 'init_hrm' ) );
 			$this->hooks();
 		}
@@ -79,11 +78,6 @@ if( !class_exists( 'Rt_HRM_Module' ) ) {
 			$this->register_custom_post( $this->menu_position );
 			$this->register_custom_statuses();
                          
-		}
-                
-                function init_help() {
-			global $rt_hrm_help;
-			$rt_hrm_help = new Rt_Hrm_Help();
 		}
 
         /**
