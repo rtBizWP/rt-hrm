c95532aa420e5d131d9731ebc7613a6e557e3582
Paresh Radadiya <paresh.radadiya@rtcamp.com>
2014-08-12 17:17:42 +0530

    check plugin dependency

--- index.php
+++ index.php
@@ -88,7 +88,7 @@ function rt_hrm_include() {
 	include_once RT_HRM_PATH_HELPER . 'rthrm-functions.php';
 
 	global $rthrm_app_autoload, $rthrm_admin_autoload, $rthrm_models_autoload, $rthrm_helper_autoload, $rthrm_calendar_autoload, $rthrm_form_autoload, $rthrm_settings_autoload;
-	$rthrm_app_autoload = new RT_WP_Autoload( RT_HRM_PATH_APP );
+        $rthrm_app_autoload = new RT_WP_Autoload( RT_HRM_PATH_APP );
 	$rthrm_admin_autoload = new RT_WP_Autoload( RT_HRM_PATH_ADMIN );
 	$rthrm_models_autoload = new RT_WP_Autoload( RT_HRM_PATH_MODELS );
 	$rthrm_helper_autoload = new RT_WP_Autoload( RT_HRM_PATH_HELPER );
@@ -108,4 +108,42 @@ function rt_hrm_init() {
 	$rt_wp_hrm = new RT_WP_HRM();
 
 }
+
 add_action( 'rt_biz_init', 'rt_hrm_init', 1 );
+
+
+function check_rt_biz_dependecy() {
+            $flag = true;
+            $used_function = array(
+                'rt_biz_sanitize_module_key',
+                'rt_biz_get_access_role_cap',
+                'rt_biz_get_person_for_wp_user',
+                'rt_biz_get_access_role_cap',
+                'rt_biz_get_wp_user_for_person',
+                'rt_biz_search_employees'
+            );
+
+            foreach ( $used_function as $fn ) {
+                if ( ! function_exists( $fn ) ) {
+                    $flag = false;
+                }
+            }
+
+            if ( ! class_exists( 'Rt_Biz' ) ) {
+                $flag = false;
+            }
+
+            if ( ! $flag ) {
+                add_action( 'admin_notices','rt_biz_admin_notice' );
+            }
+
+            return $flag;
+}
+
+    add_action( 'admin_init', 'check_rt_biz_dependecy', 1 );
+
+    function rt_biz_admin_notice() { ?>
+			<div class="updated">
+				<p><?php _e( sprintf( 'WordPress HRM : It seems that WordPress rt-biz plugin is not installed or activated. Please %s / %s it.', '<a href="'.admin_url( 'plugin-install.php?tab=search&s=rt-biz' ).'">'.__( 'install' ).'</a>', '<a href="'.admin_url( 'plugins.php' ).'">'.__( 'activate' ).'</a>' ) ); ?></p>
+			</div>
+		<?php }
