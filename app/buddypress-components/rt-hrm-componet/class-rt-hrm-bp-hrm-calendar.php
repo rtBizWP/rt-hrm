e0e62067ee2be8f7e3a2e83b2f32e863ec384b7c
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-09-10 17:33:19 +0530

    fixed the user auto complete option and added new leaves

--- app/buddypress-components/rt-hrm-componet/class-rt-hrm-bp-hrm-calendar.php
+++ app/buddypress-components/rt-hrm-componet/class-rt-hrm-bp-hrm-calendar.php
@@ -22,6 +22,84 @@ if ( ! class_exists( 'Rt_Hrm_Bp_Hrm_Calendar' ) ) {
         }
 		
 		/**
+		 * Setter method for screen id
+		 */
+		function add_screen_id( $screen_id ) {
+			$this->screen_id = $screen_id;
+		}
+		
+		/**
+         * Actions to be taken prior to page loading. This is after headers have been set.
+         * Handle calendar page event
+         * call on load-$hook
+         */
+        function page_actions() {
+			global $rt_hrm_module, $rt_hrm_attributes;
+			
+			if ( isset( $_REQUEST['form-add-leave'] ) && !empty( $_REQUEST['form-add-leave'] ) && ! is_admin() ) {
+
+				$leave_meta = $_REQUEST['post'];
+				$author = rt_biz_get_wp_user_for_person( $leave_meta['leave-user-id'] );
+
+				$args = array(
+					'meta_query' => array(
+						array(
+							'key' => 'leave-user-id',
+							'value' => $leave_meta['leave-user-id']
+						),
+						array(
+							'key' => 'leave-start-date',
+							'value' => $leave_meta['leave-start-date']
+						)
+					),
+					'post_type' => $rt_hrm_module->post_type,
+					'post_status' => 'any',
+					'nopaging' => true
+				);
+
+				$posts = get_posts($args);
+
+				if ( count($posts) > 0 ) {
+					wp_redirect( admin_url( 'edit.php?post_type=rt_leave&page=rthrm-rt_leave-calendar&message_id=1' ) );
+					die();
+				}
+
+				$newLeave = array(
+					'comment_status' =>  'closed',
+					'post_author' => $author,
+					'post_date' => date('Y-m-d H:i:s'),
+					'post_content' => $leave_meta['leave_description'],
+					'post_status' => 'pending',
+					'post_title' => ' Leave: ' . $leave_meta['leave-user'],
+					'post_type' => $rt_hrm_module->post_type,
+				);
+
+				$newLeaveID = wp_insert_post($newLeave);
+
+				if ( isset( $leave_meta[  Rt_HRM_Attributes::$leave_type_tax] ) ) {
+					wp_set_post_terms( $newLeaveID, implode( ',', array_map( 'intval', $leave_meta[Rt_HRM_Attributes::$leave_type_tax] ) ), Rt_HRM_Attributes::$leave_type_tax );
+				}
+
+				if ( isset( $_POST['leave_quota_use'] ) ) {
+					update_post_meta( $newLeaveID, '_rt_hrm_leave_quota_use', $_POST['leave_quota_use'] );
+				}
+
+                update_post_meta( $newLeaveID, 'leave-user', $leave_meta['leave-user'] );
+                update_post_meta( $newLeaveID, 'leave-user-id', $leave_meta['leave-user-id'] );
+                update_post_meta( $newLeaveID, 'leave-duration', $leave_meta['leave-duration'] );
+				update_post_meta( $newLeaveID, 'leave-start-date', $leave_meta['leave-start-date'] );
+
+				if ( $leave_meta['leave-duration'] == 'other' ){
+					update_post_meta( $newLeaveID, 'leave-end-date', $leave_meta['leave-end-date'] );
+				}else {
+					delete_post_meta( $newLeaveID, 'leave-end-date' );
+				}
+                // wp_redirect( admin_url( 'edit.php?post_type=rt_leave&page=rthrm-rt_leave-calendar' ) );
+				//die();
+			}
+		}
+		
+		/**
          *  Add hooks for calendar page like page action or render calendar
          */
         function setup_calendar() {
