cfb10bdcf35ef5c2235888d7477fe6a0c5603d94
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-09-16 14:50:54 +0530

    added conditional loading of js file

--- app/buddypress-components/rt-hrm-componet/class-rt-hrm-bp-hrm-frontend.php
+++ app/buddypress-components/rt-hrm-componet/class-rt-hrm-bp-hrm-frontend.php
@@ -36,24 +36,22 @@ if( !class_exists( 'Rt_Hrm_Bp_Hrm_Frontend' ) ) {
          */
         function load_styles_scripts() {
 			global $post, $rt_hrm_module;
-			$pagearray = array( 'rthrm-'.$rt_hrm_module->post_type.'-calendar', 'rthrm-'.$rt_hrm_module->post_type.'-dashboard' );
-			// if( ( isset( $post->post_type ) && $post->post_type == $rt_hrm_module->post_type )
-					// || ( isset( $_REQUEST['page'] ) && in_array( $_REQUEST['page'], $pagearray ) )
-					// || ( isset( $_REQUEST['post_type'] ) && $_REQUEST['post_type'] == $rt_hrm_module->post_type ) ) {
-				 //wp_enqueue_script('jquery');
-				 wp_enqueue_script('jquery-ui-core', true );
+			
+			if ( bp_is_current_component( 'hrm' ) ){
+				
+				wp_enqueue_script('jquery-ui-core', true );
 				
 
-				if ( !  wp_style_is( 'rt-jquery-ui-css' ) ) {
+				if ( ! wp_style_is( 'rt-jquery-ui-css' ) ) {
 	                wp_enqueue_style('rt-jquery-ui-css', RT_HRM_URL . 'app/assets/css/jquery-ui-1.9.2.custom.css', false, RT_HRM_VERSION, 'all');
 				}
 				wp_enqueue_style('rthrm-admin-css', RT_HRM_URL . 'app/assets/css/hrm-frontend.css', false, RT_HRM_VERSION, 'all');
 
-                if( !wp_script_is('jquery-ui-datepicker') ) {
+                if( ! wp_script_is('jquery-ui-datepicker') ) {
                     wp_enqueue_script( 'jquery-ui-datepicker' );
                 }
 
-                if( !wp_script_is('jquery-ui-autocomplete') ) {
+                if( ! wp_script_is('jquery-ui-autocomplete') ) {
                     wp_enqueue_script('jquery-ui-autocomplete', '', array('jquery-ui-widget', 'jquery-ui-position'), '1.9.2',true);
                 }
 
@@ -62,19 +60,11 @@ if( !class_exists( 'Rt_Hrm_Bp_Hrm_Frontend' ) ) {
 
 				wp_localize_script( 'rthrm-admin-js', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
                 wp_localize_script( 'rthrm-admin-js', 'adminurl', admin_url() );
-
-			//}
-
-			// if ( isset( $_REQUEST['page'] ) && $_REQUEST['page'] == 'rthrm-'.$rt_hrm_module->post_type.'-dashboard' ) {
-				wp_localize_script('rthrm-admin-js', 'rt_hrm_top_menu', 'menu-posts-'.$rt_hrm_module->post_type);
-				wp_localize_script('rthrm-admin-js', 'rt_hrm_dashboard_url', admin_url( 'edit.php?post_type='.$rt_hrm_module->post_type.'&page='.'rthrm-'.$rt_hrm_module->post_type.'-dashboard' ) );
-			// }
-
-			// if ( isset( $_REQUEST['page'] ) && $_REQUEST['page'] == 'rthrm-'.$rt_hrm_module->post_type.'-calendar' ) {
-
+				
 				wp_enqueue_style('rt-hrm-calendar-css', RT_HRM_URL . 'app/lib/rt-calendar/calendar/fullcalendar.css', false, RT_HRM_VERSION, 'all');
 				wp_enqueue_script('rt-hrm-calendar-js', RT_HRM_URL . 'app/lib/rt-calendar/calendar/fullcalendar.js','jquery', "", true);
-			// }
+
+			}
 		}
 
         /**
