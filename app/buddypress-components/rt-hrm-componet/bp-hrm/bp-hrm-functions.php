590f63b82bf97217cb20e81940b285300d593201
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-10-03 12:08:15 +0530

    fixed the pagination with sorting issue

---
+++ app/buddypress-components/rt-hrm-componet/bp-hrm/bp-hrm-functions.php
@@ -0,0 +1,55 @@
+<?php
+
+/********************************************************************************
+ * Screen Functions
+ *
+ * Screen functions are the controllers of BuddyPress. They will execute when their
+ * specific URL is caught. They will first save or manipulate data using business
+ * functions, then pass on the user to a template file.
+ */
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Description of BP HRM functions.
+ *
+ * @author kishore
+ */
+
+function get_component_root_url(){
+	global $bp;
+	foreach ( $bp->bp_nav as $nav ) {
+	  if ( $nav['slug'] == RT_HRM_BP_HRM_SLUG .'/calender' ){
+		$nav['link'] = explode("calender", $nav['link']);
+		$link = $nav['link'][0];
+	  }
+	}
+	return $link;
+}
+
+
+function add_pagination( $totalPage, $page ){
+    global $rt_hrm_bp_hrm;
+   
+    if( $totalPage > 1 ){
+                                            
+        $base = get_component_root_url().bp_current_action().'/%_%';
+        $formate = 'page/%#%';
+        if( isset( $_GET['orderby'] ) ) {
+
+                $arr_params = array( 'orderby' => $_GET['orderby'], 'order' => $_GET['order'] );
+                $base = add_query_arg( $arr_params, get_component_root_url().bp_current_action() ) .'%_%' ; 
+                $formate = '&paged=%#%';
+        }
+
+        $customPagHTML     =  '<div class="pagination" role="menubar" aria-label="Pagination"><span class="current">Page '.$page.' of '.$totalPage.'</span>'.paginate_links( array(
+        'base' => $base,
+        'format' => $formate,
+        'total' => $totalPage,
+        'current' => $page
+        )).'</div>';
+        echo $customPagHTML;
+    }
+}
+
+?>
