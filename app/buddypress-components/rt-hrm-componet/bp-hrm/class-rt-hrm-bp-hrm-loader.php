baf0799242a98df504cb25249b981fe4b404f323
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-11-06 23:28:29 +0530

    change the menu position

--- app/buddypress-components/rt-hrm-componet/bp-hrm/class-rt-hrm-bp-hrm-loader.php
+++ app/buddypress-components/rt-hrm-componet/bp-hrm/class-rt-hrm-bp-hrm-loader.php
@@ -112,7 +112,7 @@ if ( !class_exists( 'Rt_Hrm_Bp_Hrm_Loader' ) ) {
 			$main_nav = array(
 				'name' 		      => __( $this->hrm_label ),
 				'slug' 		      => $this->id,
-				'position' 	      => 84,
+				'position' 	      => 83,
 				'screen_function'     => 'bp_hrm_calender',
 				'default_subnav_slug' => 'calender',
 			);
